66997ef843957ffaed264085d0647db52c0917bb
HukuToc2288 <hukutoc2288@yandex.ru>
2021-04-16 20:23:56 +0300

    Reset to last working state (protocol changes moved to branch protobuf) Fixed incorrect id

---
+++ src/SendereCommons/Converters.java
@@ -0,0 +1,26 @@
+package SendereCommons;
+
+public class Converters {
+
+    public static byte[] longToBytes(long l) {
+        byte[] result = new byte[8];
+        for (int i = 7; i >= 0; i--) {
+            result[i] = (byte)(l & 0xFF);
+            l >>= 8;
+        }
+        return result;
+    }
+
+    public static long bytesToLong(final byte[] b) {
+        return bytesToLong(b,0);
+    }
+
+    public static long bytesToLong(final byte[] b, int offset) {
+        long result = 0;
+        for (int i = offset; i < 8+offset; i++) {
+            result <<= 8;
+            result |= (b[i] & 0xFF);
+        }
+        return result;
+    }
+}
